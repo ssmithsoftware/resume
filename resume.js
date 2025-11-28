679b7aa57d3203e13b2a8e0ef584ea6666a936b4
scott <scottadamsmith1@gmail.com>
2025-11-28 12:03:16 -0500

    Add resume.js

---
+++ resume.js
@@ -0,0 +1,13 @@
+const svgTemplate = document.getElementById('svgs')
+
+class SVGElement extends HTMLElement {
+	connectedCallback() {
+		this.replaceChildren(
+			svgTemplate.content
+				.getElementById(this.dataset.children)
+				.cloneNode(true),
+		)
+	}
+}
+
+customElements.define('s-svg', SVGElement)
