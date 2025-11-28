2f2a7cc36d92db1782027ff7a82b2ccd38189477
scott <scottadamsmith1@gmail.com>
2025-11-28 15:58:12 -0500

    Add work-company and work-time

--- resume.js
+++ resume.js
@@ -1,3 +1,33 @@
+// Update current company date
+function updateCurrentTime() {
+	const currentTime = document.querySelector('[data-current]')
+	const months = [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+		'August',
+		'September',
+		'October',
+		'November',
+		'December'
+	]
+	const now = new Date()
+
+	const year = now.getFullYear()
+	const month = now.getMonth()
+	const monthName = months[month]
+
+	currentTime.dateTime = `${year}-${String(month + 1).padStart(2, '0')}`
+	currentTime.textContent = `${monthName} ${year}`
+}
+
+document.addEventListener('DOMContentLoaded', updateCurrentTime)
+
+// Custom SVG Element used to clean inline markup
 const svgTemplate = document.getElementById('svgs')
 
 class SVGElement extends HTMLElement {
